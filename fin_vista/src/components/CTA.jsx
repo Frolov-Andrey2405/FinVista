079418d51b48b422bf7da10331b2412a6015e27f
Andrey Frolov <senior.slender2015@gmail.com>
2023-12-22 19:34:03 +0200

    Client & CTA

--- fin_vista/src/components/CTA.jsx
+++ fin_vista/src/components/CTA.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import styles from "../style";
+import Button from "./Button";
 
-const CTA = () => {
-    return (
-      <div>CTA</div>
-    )
-}
+const CTA = () => (
+  <section
+    className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}
+  >
+    <div className="flex-1 flex flex-col">
+      <h2 className={styles.heading2}>Let's try our service now</h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Feel confident - try our service now.
+        Instant registration and access to advanced features to give you convenience and efficiency in all aspects of your finances.
+        Are you ready for a future of financial comfort?
+      </p>
+    </div>
 
-export default CTA
+    <div className={`${styles.flexCenter} sm:ml-10 ml-0 mt-10`}>
+      <Button />
+    </div>
+  </section>
+);
+
+export default CTA;
